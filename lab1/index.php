222742ba63175671d301a547030c3c6487a666be
Nate Fox <fox016@gmail.com>
2014-01-21 23:41:21 +0000

    Working with dialogs

--- lab1/index.php
+++ lab1/index.php
@@ -12,7 +12,10 @@
 <head>
 	<title>Nathan Fox - Lab 1</title>
 	<link type='text/css' rel='stylesheet' href='css/index.css'>
+	<link type='text/css' rel='stylesheet' href='js/jquery-ui/css/no-theme/jquery-ui-1.10.4.custom.css'>
 	<script type='text/javascript' src='js/index.js'></script>
+	<script type='text/javascript' src='js/jquery-ui/js/jquery-1.10.2.js'></script>
+	<script type='text/javascript' src='js/jquery-ui/js/jquery-ui-1.10.4.custom.min.js'></script>
 </head>
 <body>
 	<div id='pageHeader'>
@@ -74,7 +77,7 @@
 			</tr>
 			<tr>
 				<td>Password:</td>
-				<td><input type=text id='newPassword'></td>
+				<td><input type=password id='newPassword'></td>
 			</tr>
 		</table>
 	</div>
--- lab1/js/index.js
+++ lab1/js/index.js
@@ -1,8 +1,59 @@
 function openAccountEditor()
 {
 	$("#accountEditor").dialog({
+		draggable: true,
+		title: "Create New Account",
 		modal: true,
 		width: 300,
-		height: 300,
+		height: 220,
+		resizable: false,
+		open: function()
+		{
+			$("#newEmail").val("");
+			$("#newDisplayName").val("");
+			$("#newPassword").val("");
+		},
+		close: function()
+		{
+			$(this).dialog("destroy");
+		},
+		buttons:
+		[
+			{
+				text: "Create Account",
+				id: "saveBtn",
+				click: function()
+				{
+					createAccount();
+				}
+			},
+			{
+				text: "Cancel",
+				id: "cancelBtn",
+				click: function()
+				{
+					$(this).dialog("close");
+				},
+			},
+		],
+	});
+}
+
+function createAccount()
+{
+	$.ajax({
+		type: 'POST',	
+		url: 'ajax/createAccount.php',
+		data: {
+			email: $("#newEmail").val(),
+			displayName: $("#newDisplayName").val(),
+			password: $("#newPassword").val(),
+		},
+		success: function() {
+			// TODO
+		},
+		error: function() {
+			// TODO
+		},
 	});
 }
